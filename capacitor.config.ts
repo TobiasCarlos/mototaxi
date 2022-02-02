d38ac24927f9eaaf85acd91a39303853a17e27c9
sergiomarinelli <89478880+sergiomarinelli@users.noreply.github.com>
2022-02-02 19:00:54 -0300

    first commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'C:/Program Files/Git/',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
