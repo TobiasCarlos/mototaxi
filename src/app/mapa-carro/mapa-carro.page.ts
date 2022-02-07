94a8e98bd6be696c73873b2dc218ed17cbbbc3f7
sergiomarinelli <sergiomarinellib23@gmail.com>
2022-02-07 10:40:27 -0300

    add

--- src/app/mapa-carro/mapa-carro.page.ts
+++ src/app/mapa-carro/mapa-carro.page.ts
@@ -12,6 +12,10 @@ export class MapaCarroPage implements OnInit {
   search : string;
   originMarker: any;
   destination: any;
+  map: any;
+  distance: any;
+  time: any;
+  price = 12.00;
 
   constructor(private ngZone: NgZone) {
 
@@ -19,262 +23,79 @@ export class MapaCarroPage implements OnInit {
   }
 
   ngOnInit() {
-    this.initMap();
+    this.initialize();
   }
 
-  initMap(): void {
-    //get position of user
+  // initMap(): void {
+  //   //get position of user
 
 
-      let map = new google.maps.Map(
-        document.getElementById('map') as HTMLElement,
-        {
-          center: { lat: -34.9290, lng: 138.6010 },
-          zoom: 15,
-          //remove buttons
-          disableDefaultUI: true,
-          //uber style
-          styles: [
-            {
-              featureType: 'all',
-              elementType: 'geometry',
-              stylers: [
-                {
-                  color: '#f5f5f5',
-                  lightness: '-10',
-                },
-              ],
-            },
-            {
-              featureType: 'administrative.country',
-              elementType: 'geometry',
-              stylers: [
-                {
-                  visibility: 'on',
-                },
-              ],
-            },
-            {
-              featureType: 'administrative.country',
-              elementType: 'geometry.stroke',
-              stylers: [
-                {
-                  color: '#a0a4a5',
-                },
-              ],
-            },
-            {
-              featureType: 'administrative.province',
-              elementType: 'geometry.stroke',
-              stylers: [
-                {
-                  color: '#62838e',
-                },
-              ],
-            },
-            {
-              featureType: 'landscape',
-              elementType: 'geometry.fill',
-              stylers: [
-                {
-                  color: '#dde3e3',
-                },
-              ],
-            },
-            {
-              featureType: 'landscape.man_made',
-              elementType: 'geometry.stroke',
-              stylers: [
-                {
-                  color: '#3f4a51',
-                },
-                {
-                  weight: '0.30',
-                },
-              ],
-            },
-            {
-              featureType: 'poi',
-              elementType: 'all',
-              stylers: [
-                {
-                  visibility: 'simplified',
-                },
-              ],
-            },
-            {
-              featureType: 'poi.attraction',
-              elementType: 'all',
-              stylers: [
-                {
-                  visibility: 'on',
-                },
-              ],
-            },
-            {
-              featureType: 'poi.business',
-              elementType: 'all',
-              stylers: [
-                {
-                  visibility: 'off',
-                },
-              ],
-            },
-            {
-              featureType: 'poi.government',
-              elementType: 'all',
-              stylers: [
-                {
-                  visibility: 'off',
-                },
-              ],
-            },
-            {
-              elementType: 'all',
-              stylers: [
-                {
-                  visibility: 'on',
-                },
-              ],
-            },
-            {
-              featureType: 'poi.school',
-              elementType: 'all',
-              stylers: [
-                {
-                  visibility: 'off',
-                },
-              ],
-            },
-            {
-              featureType: 'road',
-              elementType: 'all',
-              stylers: [
-                {
-                  saturation: '-100',
-                },
-                {
-                  visibility: 'on',
-                },
-              ],
-            },
-            {
-              featureType: 'road',
-              elementType: 'geometry.stroke',
-              stylers: [
-                {
-                  visibility: 'on',
-                },
-              ],
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'geometry.fill',
-              stylers: [
-                {
-                  color: '#bbcacf',
-                },
-              ],
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'geometry.stroke',
-              stylers: [
-                {
-                  lightness: '0',
-                },
-                {
-                  color: '#bbcacf',
-                },
-                {
-                  weight: '0.50',
-                },
-              ],
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'labels',
-              stylers: [
-                {
-                  visibility: 'on',
-                },
-              ],
-            },
-            {
-              featureType: 'road.highway',
-              elementType: 'labels.text',
-              stylers: [
-                {
-                  visibility: 'on',
-                },
-              ],
-            },
-            {
-              featureType: 'road.highway.controlled_access',
-              elementType: 'geometry.fill',
-              stylers: [
-                {
-                  color: '#ffffff',
-                },
-              ],
-            },
-            {
-              featureType: 'road.highway.controlled_access',
-              elementType: 'geometry.stroke',
-              stylers: [
-                {
-                  color: '#a9b4b8',
-                },
-              ],
-            },
-            {
-              featureType: 'road.arterial',
-              elementType: 'labels.icon',
-              stylers: [
-                {
-                  invert_lightness: true,
-                },
-                {
-                  saturation: '-7',
-                },
-                {
-                  lightness: '3',
-                },
-                {
-                  gamma: '1.80',
-                },
-                {
-                  weight: '0.01',
-                },
-              ],
-            },
-            {
-              featureType: 'transit',
-              elementType: 'all',
-              stylers: [
-                {
-                  visibility: 'off',
-                },
-              ],
-            },
-            {
-              featureType: 'water',
-              elementType: 'geometry.fill',
-              stylers: [
-                {
-                  color: '#a3c7df',
-                },
-              ],
-            },
-           
-          ],
+  //     let map = new google.maps.Map(
+  //       document.getElementById('map') as HTMLElement,
+  //       {
+  //         center: { lat: -34.9290, lng: 138.6010 },
+  //         zoom: 15,
+  //         //remove buttons
+  //         disableDefaultUI: true,
+  //         //uber style
+  //         //type road
+  //         mapTypeId: 'roadmap'
+  //       }
+  //     );
+
+
+
+
+
+
+  // }
+
+  initialize() {
+    /* get my location*/
+    navigator.geolocation.getCurrentPosition(position => {
+      var myLocation = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+      console.log( "teste", myLocation);
+      var lat = myLocation.lat();
+      var lng = myLocation.lng();
+
+    // Create a map centered in Pyrmont, Sydney (Australia).
+    this.map = new google.maps.Map(document.getElementById('map'), {
+      center: {lat: lat, lng: lng},
+      zoom: 15
+    });
+    this.map.markers = [];
+    this.originMarker = new google.maps.Marker({
+      position: {lat: lat, lng: lng},
+      map: this.map,
+      title: 'Localização Atual'
+    });
+
+    // Search for Google's office in Australia.
+    var request = {
+      location: this.map.getCenter(),
+      radius: '500',
+      query: 'Google Sydney'
+    };
+  
+    var service = new google.maps.places.PlacesService(this.map);
+    service.textSearch(request, this.callback);
+  });
+  }
+  
+  // Checks that the PlacesServiceStatus is OK, and adds a marker
+  // using the place ID and location from the PlacesService.
+  callback(results, status) {
+    if (status == google.maps.places.PlacesServiceStatus.OK) {
+      var marker = new google.maps.Marker({
+        map: this.map,
+        place: {
+          placeId: results[0].place_id,
+          location: results[0].geometry.location
         }
-      );
-
-
-
-
+      });
+    }
   }
+  
 
 
   event(event) {
@@ -298,7 +119,53 @@ export class MapaCarroPage implements OnInit {
 
 
   calcRoute(item: any){
-    this.search = ""
-    console.log(item)
+    this.search = "";
+    var request = {
+      location: this.map.getCenter(),
+      radius: '500',
+      query: item.description
+    };
+  
+    var service = new google.maps.places.PlacesService(this.map);
+    service.textSearch(request, (results, status) => {
+      /* pegar lat long */
+      var lat = results[0].geometry.location.lat();
+      var lng = results[0].geometry.location.lng();
+      console.log(lat, lng)
+      /* enviar centro do mapa para lat long */
+      this.map.setCenter({lat: lat, lng: lng});
+      /* criar marcador */
+
+      this.map.markers.push(new google.maps.Marker({
+        map: this.map,
+        position: {lat: lat, lng: lng}
+      }));
+
+      /* criar rota do origin marker ate o marker */
+      var directionsService = new google.maps.DirectionsService;
+      var directionsDisplay = new google.maps.DirectionsRenderer;
+      directionsDisplay.setMap(this.map);
+      directionsService.route({
+        origin: this.originMarker.getPosition(),
+        destination: {lat: lat, lng: lng},
+        travelMode: 'DRIVING'
+      }, (response, status) => {
+        if (status === 'OK') {
+          directionsDisplay.setDirections(response);
+          /* pegar distancia de pontos */
+          this.distance= response.routes[0].legs[0].distance.text;
+          this.time = response.routes[0].legs[0].duration.text;
+          var distanceCal = parseInt(this.distance)
+          console.log(distanceCal)
+          if(distanceCal>5){
+            distanceCal = distanceCal - 5;
+            this.price = distanceCal*2;
+          }
+        } else {
+          window.alert('Directions request failed due to ' + status);
+        }
+      }
+      );
+    });
   }
 }
