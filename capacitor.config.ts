a1793de0526230f221d9935fcaba5148c0e42b4b
Tobias Bandeira Carlos Soares <tobiasbandeiracarlos@gmail.com>
2022-02-08 11:12:41 -0300

    nome app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.tcb.taxi',
-  appName: 'C:/Program Files/Git/',
+  appName: 'Mob',
   webDir: 'www',
   bundledWebRuntime: false,
 };
